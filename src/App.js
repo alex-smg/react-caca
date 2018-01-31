c08fb073abe47681bbb000a647bcdfed649ba6f2
alex-smg <35601507+alex-smg@users.noreply.github.com>
2018-02-01 00:56:26 +0100

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, {Component} from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends React.Component {
@@ -16,6 +15,7 @@ class App extends React.Component {
         this.setState({value: event.target.value});
     };
 
+
     componentDidMount() {
 
 
@@ -25,54 +25,55 @@ class App extends React.Component {
                 this.setState({seriesList: seriesListDepuisFichier});
 
             })
-            .catch(function (error) {
-                console.log(error);
-            })
-            .then(function () {
-                alert("j'ai fait ce que j'ai pu");
-            });
+        fetch('seriesEpisodesList.json',{})
+            .then(response => response.json())
+            .then(seriesListDepuisFichier => {
+                this.setState({seriesEpisodesList: seriesListDepuisFichier});
 
-    }
+            })
+
+
+
+    };
+
 
     render() {
-        console.log(this.state.seriesList.id)
+
         return (
+
             <div>
                 <input type="text"  value={this.state.value} onChange={this.handleChange}/>
 
                 <ul>
                     {   this.state.value !== "" ? this.state.seriesList.filter(a => a.seriesName.toLowerCase().indexOf(this.state.value)>-1)
-                        .map(item => <li key={item.id}>{item.seriesName}</li>)
-                        : <li>FUCK LES MOTIONS !</li>
-                    }
-                </ul>
-                <ul>
-                    {this.state.seriesEpisodesList.filter(b => b.this.state.seriesEpisodesList.serie_id == this.state.seriesList.id)
-                            .map(item => <li key={item.id}>{item.episodeName}</li>)
+                            .map((item) => <li key={item.id}>{item.seriesName}
+
+                                <ul>
+                                    {
+                                        this.state.seriesEpisodesList.filter(
+                                            b => b.serie_id == item.id
+                                        ).map(episode => episode.episodes_list.filter(
+                                            c => c.episodeName
+                                            ).map(name => <li>{name.episodeName}</li>)
+                                        )
 
 
+                                    }
+                                </ul>
+                            </li>)
+
+
+
+
+                        : <li>Nom de la série</li>
                     }
+
                 </ul>
 
             </div>
-
-
         )
-
-
     }
-
-
-
-
-
-
-
-
-
 }
-
-
 
 
 export default App;
